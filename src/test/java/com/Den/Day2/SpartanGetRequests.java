7a29731359b97b9cd5dec7a682d2b3b008a385f3
Dst07032023 <bovari.deonisis@gmail.com>
2024-03-19 14:55:56 +0100

    first test case done

---
+++ src/test/java/com/Den/Day2/SpartanGetRequests.java
@@ -0,0 +1,53 @@
+package com.Den.Day2;
+
+import com.sun.org.apache.xpath.internal.operations.And;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import javafx.beans.binding.When;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SpartanGetRequests {
+
+    String baseUrl = "http://52.207.61.129:8000";
+
+//    Given Accept type application/json
+//    When user send GET request to api/spartans end point
+//    Then status code must 200
+//    And response Content Type must be application/json
+//    And response body should include spartan result
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when()
+                        .get(baseUrl+"/api/spartans");
+
+        //printing status code from response object
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //printing response content type from response object
+        System.out.println("response.contentType() = " + response.contentType());
+
+        //print whole result body
+        response.prettyPrint();
+
+        //how to do API testing then?
+        Assertions.assertEquals(response.statusCode(),200);
+
+        //verify content type is application/json
+        Assertions.assertEquals(response.contentType(),"application/json");
+
+
+    }
+
+    /*
+    Given accept type application/json
+    When user sends a get request to api/spartans/3
+    Then status code should be 200
+    And content type should be application/json
+    And json body should contain Fidole
+     */
+}
