798101eb298ecffa4f88591c1be7a33c19482f7f
Dst07032023 <bovari.deonisis@gmail.com>
2024-03-20 12:57:47 +0100

    SpartanTestsWithPath class added

--- src/test/java/com/Den/Day3/ORDSApiTestsWithParameters.java
+++ src/test/java/com/Den/Day3/ORDSApiTestsWithParameters.java
@@ -39,6 +39,26 @@ public class ORDSApiTestsWithParameters {
          assertTrue(response.body().asString().contains("United States of America"));
 
          response.prettyPrint();
-
     }
+
+    /*
+    Send a GET request to employees and get only employees who work as a IT_PROG
+     */
+
+    @DisplayName("GET request to /employees with Query Param")
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+                .when()
+                .get("/employees");
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.header("Content-Type"));
+
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+        response.prettyPrint();
+    }
+
+
 }
