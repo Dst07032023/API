b79dea095c09e8dd2cf1a41dfa948ac6b97a3302
Dst07032023 <bovari.deonisis@gmail.com>
2024-03-25 14:22:15 +0100

    Day4:ORDSApiWithJsonPath added

--- src/test/java/com/Den/Day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/Den/Day4/ORDSApiWithJsonPath.java
@@ -32,6 +32,25 @@ public class ORDSApiWithJsonPath extends HRTestBase {
         //items.country_id
         List<String> allCountryIds = jsonPath.getList("items.country_id");
         System.out.println(allCountryIds);
+
+        //get all country names where their region id is equal to 2
+        List<String> countryNameWithRegionId2 = jsonPath.getList("items.findAll {it.region_id==2}.country_name");
+        System.out.println(countryNameWithRegionId2);
     }
 
+    @DisplayName("Get request/employees with param")
+    @Test
+    public void test2(){
+        //we added limit query param to get 107 employees
+        Response response = given().queryParam("limit",107)
+                .when().get("/employees");
+
+        JsonPath jsonPath = response.jsonPath();
+
+        //get me all email of employees who is working as IT_PROG
+        List<String> employeeITProgs = jsonPath.getList("items.findAll {it.job_id==\"IT_PROG\"}.email");
+        System.out.println(employeeITProgs);
+
+        //get me first name of employees who is making more than 1000
+    }
 }
