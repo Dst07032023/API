57e9c3cfcccd7aff8a63b28e3cd3d842653c2379
Dst07032023 <bovari.deonisis@gmail.com>
2024-03-28 15:36:14 +0100

    Day6: lombok dependency added

--- src/test/java/com/Den/pojo/Link.java
+++ src/test/java/com/Den/pojo/Link.java
@@ -1,30 +1,18 @@
 package com.Den.pojo;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Link {
     private String rel;
     private String href;
 
-    @Override
-    public String toString() {
-        return "Link{" +
-                "rel='" + rel + '\'' +
-                ", href='" + href + '\'' +
-                '}';
-    }
 
-    public String getRel() {
-        return rel;
-    }
-
-    public void setRel(String rel) {
-        this.rel = rel;
-    }
-
-    public String getHref() {
-        return href;
-    }
-
-    public void setHref(String href) {
-        this.href = href;
-    }
 }
