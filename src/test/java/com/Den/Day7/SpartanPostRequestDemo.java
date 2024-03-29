29971070400b92f535e1da2b1bcfd15783ae6c66
Dst07032023 <bovari.deonisis@gmail.com>
2024-03-29 11:17:18 +0100

    Day7: SpartanPostRequestDemo.java

---
+++ src/test/java/com/Den/Day7/SpartanPostRequestDemo.java
@@ -0,0 +1,26 @@
+package com.Den.Day7;
+
+import com.Den.utilities.SpartanTestBase;
+
+public class SpartanPostRequestDemo extends SpartanTestBase {
+
+    /*
+    Given accept type and Content type is JSON
+    And request json body is:
+    {
+        "gender":"Male",
+        "name":"Severus",
+        "phone":8877445596
+     }
+     When user POST request to'/api/spartans'
+     Then status code 201
+     And content type should be application/json
+     And json payload/response/body should contain:
+     "A Spartan is Born!" message
+     and same data what is posted
+     */
+
+
+
+
+}
--- src/test/java/com/Den/pojo/Regions.java
+++ src/test/java/com/Den/pojo/Regions.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Getter //from lombok dependency
 @Setter //from lombok dependency
 @ToString //from lombok dependency
-@JsonIgnoreProperties(ignoreUnknown = true) //this is from jackson
+@JsonIgnoreProperties(ignoreUnknown = true) //this is from jackson dependency
 public class Regions {
 
     private List<Region> items;
