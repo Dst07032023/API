625dc9f44225bd7b86c46f0706316e473ae7a3fd
Dst07032023 <bovari.deonisis@gmail.com>
2024-03-30 16:32:56 +0100

    Day8: New SpartanAuthTestBase.java and SpartanWithAuthTests.java admin test is done

--- src/test/java/com/Den/Day8/SpartanWithAuthTests.java
+++ src/test/java/com/Den/Day8/SpartanWithAuthTests.java
@@ -1,4 +1,37 @@
 package com.Den.Day8;
 
-public class SpartanWithAuthTests {
+import com.Den.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanWithAuthTests extends SpartanAuthTestBase {
+
+    @DisplayName("GET /api/spartans as a public user(guest) expected 401")
+    @Test
+    public void test1(){
+        given().accept(ContentType.JSON)
+               .when()
+        .get("/api/spartans")
+                .then().statusCode(401)
+                .log().all();
+    }
+
+    @DisplayName("GET/api/spartans as admin user expected 200")
+    @Test
+    public void testAdmin(){
+        //how to pass admin as a username and password?
+        given()
+                .auth().basic("admin","admin")
+                .and().accept(ContentType.JSON)
+        .when()
+                .get("/api/spartans")
+        .then()
+                .statusCode(200)
+                .log().all();
+    }
 }
